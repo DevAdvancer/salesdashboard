3ea85e1ab776aa4e09990d47ea7a7f2034ddc46b
devadvancer <abhirupkumar001@gmail.com>
2026-07-28 14:56:22 -0400

    fix: resolve build error with pino-logflare by using pino transport

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,50 +1,33 @@
 import pino from 'pino';
-import { createPinoBrowserSend, createWriteStream } from 'pino-logflare';
 
 const apiKey = process.env.LOGFLARE_API_KEY || process.env.NEXT_PUBLIC_LOGFLARE_API_KEY || '';
 const sourceToken = process.env.LOGFLARE_SOURCE_TOKEN || process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN || '';
 
 const isEnabled = Boolean(apiKey && sourceToken);
 
-let stream;
-let send;
-
-if (isEnabled) {
-  // Try/catch just in case we are in an environment where createWriteStream fails
-  try {
-    stream = createWriteStream({
-      apiKey,
-      sourceToken
-    });
-  } catch (e) {
-    // ignore
-  }
-
-  try {
-    send = createPinoBrowserSend({
-      apiKey,
-      sourceToken
-    });
-  } catch (e) {
-    // ignore
-  }
-}
-
 export const logger = pino(
   {
     browser: {
-      transmit: send ? {
-        level: 'info',
-        send: send,
-      } : undefined
+      asObject: true
     },
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
     base: {
       env: process.env.NODE_ENV,
       revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
     },
-  },
-  stream
+    ...(isEnabled && typeof window === 'undefined'
+      ? {
+          transport: {
+            target: 'pino-logflare',
+            options: {
+              apiKey,
+              sourceToken,
+            },
+          },
+        }
+      : {}),
+  }
 );
 
 export default logger;
+
